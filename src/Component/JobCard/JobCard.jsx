c6df9e9a079a9d50a0887f6c7f94b93b7e48f5b7
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-05-12 11:17:24 +0600

    home page jobs categry section all jobs added

--- src/Component/JobCard/JobCard.jsx
+++ src/Component/JobCard/JobCard.jsx
@@ -1,18 +1,66 @@
-const JobCard = ({job}) => {
-    const {userName, jobTitle, jobPostingDate, applicationDeadline, salaryRange, jobApplicantsNumber, } = job;
+import { FaPeopleGroup } from "react-icons/fa6";
+import { IoArrowForwardSharp } from "react-icons/io5";
+import { MdOutlineDateRange } from "react-icons/md";
+
+const JobCard = ({ job }) => {
+  const {
+    userName,
+    jobTitle,
+    jobPostingDate,
+    applicationDeadline,
+    salaryRange,
+    jobApplicantsNumber,
+    jobCategory,
+  } = job;
   return (
-    <div className=" border-[1px] rounded-md  bg-base-100 ">
+    <div className=" border-[1px] hover:shadow-lg rounded-md  bg-base-100 ">
       <div className="card-body">
-        <p className=" ">Posted By: <span className="text-lg font-semibold">{userName}</span></p>
-        <h2 className="card-title text-3xl">{jobTitle}</h2>
-        <div className=" flex">
-        <p>Posted Date: <span className="text-lg font-semibold">{jobPostingDate}</span></p>
-        <p>Deadline: {applicationDeadline}</p>
+        <div className=" flex justify-between">
+        <p className=" ">
+          Posted By: <span className="text-lg font-semibold">{userName}</span>
+        </p>
+        <p className={`${jobCategory === "Hybrid" ? ' bg-green-200 p-1 text-center rounded-full font-semibold text-green-500' : ''}${jobCategory === "Remote" ? ' bg-pink-200 p-1 text-center rounded-full font-semibold text-pink-500' : ''}${jobCategory === "On Site" ? ' bg-yellow-100 p-1 text-center rounded-full font-semibold text-blue-500' : ''}${jobCategory === "Part Time" ? ' bg-blue-100 p-1 text-center rounded-full font-semibold text-blue-500' : ''}`}>
+  {jobCategory}
+</p>
         </div>
+        <h2 className="card-title text-3xl">{jobTitle}</h2>
+        <div className=" flex justify-between mt-3">
+          <div className=" flex items-center gap-1">
+          <p className=" text-lg text-blue-500">
+            <MdOutlineDateRange />
+          </p>
+          <p>
+            Posted Date:{" "}
+            <span className=" font-semibold">{jobPostingDate}</span>
+          </p>
+          </div>
+          <div className=" flex items-center gap-1">
+            <p className=" text-lg text-blue-500">
+              <MdOutlineDateRange />
+            </p>
+            <p>
+              Deadline:{" "}
+              <span className=" font-semibold">{applicationDeadline}</span>
+            </p>
+          </div>
+        </div>
+        <div className=" flex">
+        <div className=" flex items-center gap-1">
+        <p className=" text-lg text-blue-500">
+        <FaPeopleGroup />
+            </p>
         <p>Applied People: {jobApplicantsNumber}</p>
+        </div>
+        <div>
+
+        </div>
+        </div>
         <div className="card-actions justify-between items-center">
-            <h3 className="text-xl ">Salary: <span className=" font-medium text-2xl ">${salaryRange}</span></h3>
-          <button className="btn btn-primary">Buy Now</button>
+          <h3 className="text-xl ">
+            Salary:{" "}
+            <span className=" font-medium text-2xl ">${salaryRange}</span>
+          </h3>
+          <button className=" text-lg text-blue-500 border-2 p-2 rounded-full px-3 border-blue-300 flex items-center gap-2">View Details <span className=" text-xl "><IoArrowForwardSharp /></span></button>
         </div>
       </div>
     </div>
--- src/Component/JobCategory/JobCategory.jsx
+++ src/Component/JobCategory/JobCategory.jsx
@@ -1,9 +1,15 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import JobCard from '../JobCard/JobCard';
+import { Link } from 'react-router-dom';
 
 const JobCategory = ({jobs}) => {
-    console.log(jobs);
+    const remoteJobs = jobs.filter(job => job.jobCategory == "Remote");
+    const onSiteJobs = jobs.filter(job => job.jobCategory == "On Site");
+    const partTimeJobs = jobs.filter(job => job.jobCategory == "Part Time");
+    const hybridJobs = jobs.filter(job => job.jobCategory == "Hybrid");
+    const allJobs = jobs.slice(0, 9);
+    console.log(allJobs);
     return (
         <div className=' '>
             <div className=' flex flex-col items-center mb-16'>
@@ -19,25 +25,46 @@ const JobCategory = ({jobs}) => {
                     <Tab className="cursor-pointer py-4 px-8 bg-blue-100 border rounded-r-2xl text-xl font-semibold">Hybrid</Tab>
                 </TabList>
 
+                <div className=' p-5 m-3 rounded-xl'>
                 <TabPanel>
-                    <div className=' grid grid-cols-3 gap-3'>
+                    <div className=' grid grid-cols-3 gap-5'>
                         {
-                            jobs.map(job=> <JobCard key={job._id} job={job}></JobCard>)
+                            allJobs.map(job=> <JobCard key={job._id} job={job}></JobCard>)
+                        }
+                    </div>
+                    <div className=' flex justify-center mt-7'>
+                    <Link to="alljobs" className=' btn btn-outline text-lg text-blue-300 '>View All Jobs</Link>
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                <div className=' grid grid-cols-3 gap-3'>
+                        {
+                            onSiteJobs.map(job=> <JobCard key={job._id} job={job}></JobCard>)
                         }
                     </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                <div className=' grid grid-cols-3 gap-3'>
+                        {
+                            remoteJobs.map(job=> <JobCard key={job._id} job={job}></JobCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 3</h2>
+                <div className=' grid grid-cols-3 gap-3'>
+                        {
+                            partTimeJobs.map(job=> <JobCard key={job._id} job={job}></JobCard>)
+                        }
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 4</h2>
+                <div className=' grid grid-cols-3 gap-3'>
+                        {
+                            hybridJobs.map(job=> <JobCard key={job._id} job={job}></JobCard>)
+                        }
+                    </div>
                 </TabPanel>
-                <TabPanel>
-                    <h2>Any content 5</h2>
-                </TabPanel>
+                </div>
             </Tabs>
         </div>
     );
